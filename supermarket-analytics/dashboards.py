d5916b865bd28a2c771a4b8d920ff4986bbb8792
mayan.dev <mayangabriel654@gmail.com>
2025-07-24 20:53:45 -0300

    feat: Adição de Arquivos

--- supermarket-analytics/dashboards.py
+++ supermarket-analytics/dashboards.py
@@ -4,12 +4,6 @@ import plotly.express as px
 
 st.set_page_config(layout="wide")
 
-# Com uma visão mensal
-#faturamento por unidade… 
-# tipo de produto mais vendido, contribuição por filial,
-#Desempenho das forma de pagamento…
-#Como estão as avaliações das filiais?
-
 df = pd.read_csv("supermarket_sales.csv", sep=";", decimal=",")
 df["Date"] = pd.to_datetime(df["Date"])
 df=df.sort_values("Date")
